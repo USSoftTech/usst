9b1765ec005f355e75fa685e9c636c81d618af72
kitdo <kitdo@InnasSurface>
2018-02-13 09:44:05 -0500

    created DynamicWeb2

--- DynamicWeb2/src/main/java/com/usst/controller/MainController.java
+++ DynamicWeb2/src/main/java/com/usst/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.usst.controller;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,18 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class BaseController {
+public class MainController {
 
 	private static int counter = 0;
 	private static final String VIEW_INDEX = "index";
-	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
 
 		model.addAttribute("message", "Welcome");
 		model.addAttribute("counter", ++counter);
-		logger.debug("[welcome] counter : {}", counter);
 
 		// Spring uses InternalResourceViewResolver and return back index.jsp
 		return VIEW_INDEX;
@@ -31,7 +28,7 @@ public class BaseController {
 
 		model.addAttribute("message", "Welcome " + name);
 		model.addAttribute("counter", ++counter);
-		logger.debug("[welcomeName] counter : {}", counter);
+
 		return VIEW_INDEX;
 
 	}
